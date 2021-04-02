f89f3299e3ad91ad376e0a9349aab998b3e3b395
TepNik <tepelin.nikita@gmail.com>
2021-04-02 15:39:16 +0300

    Added script to verify ERC721Main

---
+++ scripts/ERC721Main.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+truffle-flattener ./contracts/ERC721Main.sol > output.sol
+grep -v "// SPDX-License-Identifier: MIT" output.sol > temp && mv temp output.sol
+sed -i '1 i // SPDX-License-Identifier: MIT' output.sol
